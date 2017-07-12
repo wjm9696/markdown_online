1a09546f7173d88e85689bcc10e5119841a7c9a7
Jieming Wei <jwei@paloaltonetworks.com>
2017-07-11 22:36:12 -0700

    craete db

--- server/serverAccount/userOp.js
+++ server/serverAccount/userOp.js
@@ -9,6 +9,7 @@ class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.picture = picture;
+        this.files = [];
     }
 };
 
@@ -18,21 +19,22 @@ class MarkdownFile {
         this.fileContent = fileContent;
         this.fileID = fileID;
         this.followerIds = followerIds;
+        this.files = [];
     }
 }
 
-const getUser = async function(user) {
+const getUser = async function (user) {
     const db = await MongoClient.connect(url);
     const result = await db.collection("users").findOne({ 'email': user.email });
     return result;
 };
-const registerUserInfo = async function(user) {
+const registerUserInfo = async function (user) {
     const db = await MongoClient.connect(url);
     await db.collection("users").insert(user);
     console.log(user, "inserted");
 };
 
-const getUserId = async function(user) {
+const getUserId = async function (user) {
     // const db = await MongoClient.connect(url);
     // const result = await db.collection("users").findOne({ 'email': user.email });
     // return result._id.toString();
@@ -41,14 +43,23 @@ const getUserId = async function(user) {
     return id;
 };
 
-const getFile = async function(fileId) {
+const getUserEmail = async function (user) {
+    // const db = await MongoClient.connect(url);
+    // const result = await db.collection("users").findOne({ 'email': user.email });
+    // return result._id.toString();
+    const dbuser = await getUser(user)
+    return dbuser.email
+};
+
+const getFile = async function (fileId) {
     const db = await MongoClient.connect(url);
     const result = await db.collection("files").findOne({ 'fileID': fileId });
     return result;
 };
 
-const setUpApi = async function(app) {
-    app.post('/put/signin', function(req, res) {
+const setUpApi = async function (app) {
+    app.post('/put/signin', function (req, res) {
+        try{
         let userToken = req.body.userToken;
         let userInfo = googleTokenValidation.getUserInfo(userToken);
         if (userInfo.email != null) {
@@ -62,42 +73,53 @@ const setUpApi = async function(app) {
                 }
             })
         }
+        }catch(e){
+            throw e;
+        }
 
     })
 
-    app.post('/put/save', async function(req, res) {
+    app.post('/put/save', async function (req, res) {
         //TODO: optimize this
-        const userToken = req.body.userToken;
-        const fileTitle = req.body.fileTitle;
-        const fileContent = req.body.fileContent;
-        const fileID = req.body.fileID;
-        const userInfo = googleTokenValidation.getUserInfo(userToken);
-        if (userInfo.email == null) {
-            res.json({ message: 'error, user not log in' });
-            res.end();
-        } else {
-            const db = await MongoClient.connect(url);
-            const user = await db.collection("users").findOne({ 'email': userInfo.email });
-            const userID = await getUserId(user);
-            console.log(userID);
-            const dbfile = await getFile(fileID)
-            if(dbfile){
+        try {
+            const userToken = req.body.userToken;
+            const fileTitle = req.body.fileTitle;
+            const fileContent = req.body.fileContent;
+            const fileID = req.body.fileID;
+            const userInfo = googleTokenValidation.getUserInfo(userToken);
+            if (userInfo.email == null) {
+                res.json({ message: 'error, user not log in' });
+                res.end();
+            } else {
                 const db = await MongoClient.connect(url);
-                const followerIds = dbfile.followerIds;
-                dbfile.fileContent = fileContent;
-                dbfile.fileTitle = fileTitle;
-                if(followerIds.includes(userID)){
-                    db.collection("files").update({fileID: fileID}, dbfile);
-                }else{
-                    followerIds.push(userID);
-                    db.collection("files").update({fileID: fileID}, dbfile);
+                const user = await db.collection("users").findOne({ 'email': userInfo.email });
+                const userEmail = await getUserEmail(user);
+                console.log(user);
+                const dbfile = await getFile(fileID)
+                if (dbfile) {
+                    const db = await MongoClient.connect(url);
+                    const followerIds = dbfile.followerIds;
+                    dbfile.fileContent = fileContent;
+                    dbfile.fileTitle = fileTitle;
+                    if (followerIds.includes(userEmail)) {
+                        db.collection("files").update({ fileID: fileID }, dbfile);
+                    } else {
+                        followerIds.push(userEmail);
+                        db.collection("files").update({ fileID: fileID }, dbfile);
+                        const user = await db.collection('users').findOne({ 'email': userEmail });
+                        user.files.push(fileID);
+                    }
+                    console.log(db);
+                } else {
+                    const newFile = new MarkdownFile(fileTitle, fileContent, fileID, [userEmail]);
+                    const db = await MongoClient.connect(url);
+                    user.files.push(fileID);
+                    await db.collection("users").update({ email: userEmail }, user);
+                    await db.collection("files").insert(newFile);
                 }
-                console.log(db);
-            }else{
-                const newFile = new MarkdownFile(fileTitle, fileContent, fileID, [userID]);
-                const db = await MongoClient.connect(url);
-                await db.collection("files").insert(newFile);
             }
+        } catch (e) {
+            throw e;
         }
     })
 }
