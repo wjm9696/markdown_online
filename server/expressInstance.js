8adae60f9f0aaf57418a165369b0faace10c6fcb
Jieming Wei <jwei@paloaltonetworks.com>
2017-07-17 01:30:56 -0700

    fix pdf bug

--- server/expressInstance.js
+++ server/expressInstance.js
@@ -33,7 +33,7 @@ app.use(function (req, res, next) {
     //res.send(200);
     //return next(new Error());
 });
-//app.use(allowCrossDomain);
+app.use(allowCrossDomain);
 app.use(cors());
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded()); // to support URL-encoded bodies
