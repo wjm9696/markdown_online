72532edc0c8dc7b235ac9ca402c7d7a9d06f486d
Jieming Wei <jwei@paloaltonetworks.com>
2017-07-17 00:50:32 -0700

    fix pdf bug

--- server/expressInstance.js
+++ server/expressInstance.js
@@ -28,6 +28,7 @@ var allowCrossDomain = function(req, res, next) {
 app.use(function (req, res, next) {
     console.log("signin"+req.body);
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.send(200);
     return next(new Error());
