e681d4ed4103e272df4cf4da53e302458f44afb2
Jieming Wei <jwei@paloaltonetworks.com>
2017-07-17 01:29:47 -0700

    fix pdf bug

--- server/expressInstance.js
+++ server/expressInstance.js
@@ -30,8 +30,8 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.send(200);
-    return next(new Error());
+    //res.send(200);
+    //return next(new Error());
 });
 //app.use(allowCrossDomain);
 app.use(cors());
